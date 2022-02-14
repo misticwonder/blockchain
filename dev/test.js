7287b448d49c958c34aa4162aa7c3f4ff70c1bd2
Andrey Korovin <misticwonder@gmail.com>
2022-02-14 20:55:26 +0200

    create block

---
+++ dev/test.js
@@ -0,0 +1,9 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf');
+bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf');
+bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf');
+
+console.log(bitcoin);
