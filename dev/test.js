a2486cb5b64a4c350ef2ae3d44db22b03d809632
Andrey Korovin <misticwonder@gmail.com>
2022-02-21 09:39:04 +0200

    create transaction

--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,8 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf');
-bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf');
+console.log(bitcoin.createNewTransaction(100,'AK','Krosha'));
 bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf');
 
+console.log(bitcoin.createNewTransaction(200,'AK','Krosha'));
 console.log(bitcoin);
